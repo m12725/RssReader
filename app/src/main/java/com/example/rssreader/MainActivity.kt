2b9263170800e3617d0df7cc51510461f2721d53
Milly Kato <cmilly94@gmail.com>
2019-04-12 17:40:43 +0900

    PUSH通知＆JOBスケジューラの実装

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.rssreader
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.ServiceConnection
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.support.customtabs.CustomTabsIntent
 import android.support.v4.app.LoaderManager
 import android.support.v4.content.Loader
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
 
@@ -17,6 +23,20 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
 
         //　ローダーを呼び出す
         supportLoaderManager.initLoader(1, null, this as LoaderManager.LoaderCallbacks<Rss>)
+
+        // 通知チャンネルを作成する
+        createChannel(this)
+
+        // 定期的に新しい記事が無いかをチェックするジョブ
+        val fetchJob = JobInfo.Builder(1, ComponentName(this, PollingJob::class.java))
+            .setPeriodic(TimeUnit.HOURS.toMillis(6)) //6時間ごとに実行
+            .setPersisted(true) // 端末を再起動しても有効
+        //ネットワーク接続されていること
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            .build()
+
+        //ジョブを登録する
+        getSystemService(JobScheduler::class.java).schedule(fetchJob)
     }
 
     // ローダーが要求されたときに呼ばれる
--- app/src/main/java/com/example/rssreader/Notification.kt
+++ app/src/main/java/com/example/rssreader/Notification.kt
@@ -1,4 +1,4 @@
-/* package com.example.rssreader
+package com.example.rssreader
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -7,12 +7,11 @@ import android.content.Context
 import android.content.Intent
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
-import android.view.Gravity.apply
 
 //　チャンネルID
 private const val CHANNEL_ID = "update_channel"
 // 通知ID
-private const val NOTIFICAITON_ID = 1
+private const val NOTIFICATION_ID = 1
 // PendingIntent用のリクエストコード
 private const val REQUEST_CODE = 1
 
@@ -22,39 +21,37 @@ fun createChannel(context: Context) {
     //通知チャンネルを作成する
     val channel = NotificationChannel(
         CHANNEL_ID, // チャンネルID
-    "新規記事",　// チャンネル名
-    NotificationManager.IMPORTANCE_DEFAULT // チャンネルの重要度
-        ).apply {
-        enableLights(false) // LED光らせるか
+        "新規記事",//チャンネル名
+        NotificationManager.IMPORTANCE_DEFAULT // チャンネルの重要度
+    ).apply {
+        enableLights(true) // LED光らせるか
         enableVibration(true) // バイブレーションを行うか
         setShowBadge(true) // アイコンにバッジをつけるか
     }
 
     //端末にチャンネルを登録する
-    val manager = context.getSystemService(NotificationManager:class.java)
+    val manager = context.getSystemService(NotificationManager::class.java)
     manager.createNotificationChannel(channel)
-
-    // 最新通知を行う
-    fun notifyUpdate(context: Context) {
-        //通知をタップしたときに起動する画面
-        val intent = Intent(context, MainActivity::class.java)
-        // 通知に設定するために、PendingIntentにする
-        val pendingIntent = PendingIntent.getActivity(
-            context, REQUEST_CODE, intent, PendingIntent.FLAG_ONE_SHOT)
-
-        //通知を作成する
-        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setContentTitle("記事が更新されました") //通知のタイトル
-            .setContentText("新しい記事をチェックしましょう") //　通知のテキスト
-            .setContentIntent(pendingIntent) // 通知タップ時に起動するIntent
-            .setSmallIcon(R.drawable.ic_notification) // 通知のアイコン
-            .setAutoCancel(true) // 通知をタップしたら、その通知を消す
-            .build()
-
-        //通知する
-        NotificationManagerCompat.from(context).notify(
-            NOTIFICAITON_ID, notification)
-    }
 }
 
-        */
+//最新通知を行う
+fun notifyUpdate(context: Context) {
+    //通知ををタップした時に起動する画面
+    val intent = Intent(context, MainActivity::class.java)
+    //通知に設定するために、PendingIntentにする
+    val pendingIntent = PendingIntent.getActivity(
+        context, REQUEST_CODE, intent, PendingIntent.FLAG_ONE_SHOT)
+
+    //通知を製作する
+    val notification =  NotificationCompat.Builder(context, CHANNEL_ID)
+        .setContentTitle("記事が更新されました")
+        .setContentText("新しい記事をチェックしましょう")
+        .setContentIntent(pendingIntent) //通知をタップ時に起動するIntent
+        .setSmallIcon(R.drawable.ic_notification)
+        .setAutoCancel(true) //通知をタップしたら、その通知を消す
+        .build()
+
+    //通知する
+    NotificationManagerCompat.from(context).notify(
+        NOTIFICATION_ID, notification)
+}
